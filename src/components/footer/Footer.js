f17055c86c18428b487c5f588f3479a748310d6c
Divykant <divykant1921107@akgec.ac.in>
2023-09-24 00:41:53 +0530

    second commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -16,36 +16,75 @@
 
 import React from "react";
 import "./Footer.css";
-
+import Logo from "../../assets/logo.png";
+import Fb from "../../assets/fb.png";
+import Insta from "../../assets/insta.png";
+import Twitter from "../../assets/twitter.png";
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-containerDiv">
         <div className="footer-containerDiv-section">
-          <h3>Logo</h3>
+          <img
+            src={Logo}
+            className="footer-containerDiv-sectionLogo"
+            alt="logo"
+          />
           {/* Add your logo image here */}
         </div>
         <div className="footer-containerDiv-section">
-          <h3>Contact</h3>
-          <p>Address: 123 Main Street</p>
-          <p>Email: example@example.com</p>
-          <p>Phone: +123 456 7890</p>
+          <h3 className="footer-containerDiv-sectionTitle">Contact</h3>
+          <p className="footer-containerDiv-sectionListItem">
+            Address: 123 Main Street
+          </p>
+          <p className="footer-containerDiv-sectionListItem">
+            Email: example@example.com
+          </p>
+          <p className="footer-containerDiv-sectionListItem">
+            Phone: +123 456 7890
+          </p>
         </div>
         <div className="footer-containerDiv-section">
-          <h3>Links</h3>
-          <ul>
-            <li>About Us</li>
-            <li>Latest Articles</li>
-            <li>Header Link</li>
-            <li>More</li>
+          <h3 className="footer-containerDiv-sectionTitle">Links</h3>
+          <ul className="footer-containerDiv-sectionList">
+            <li className="footer-containerDiv-sectionListItem">About Us</li>
+            <li className="footer-containerDiv-sectionListItem">
+              Latest Articles
+            </li>
+            <li className="footer-containerDiv-sectionListItem">Header Link</li>
+            <li className="footer-containerDiv-sectionListItem">More</li>
           </ul>
         </div>
         <div className="footer-containerDiv-section">
-          <h3>Social Media</h3>
-          <ul>
-            <li>Facebook</li>
-            <li>Twitter</li>
-            <li>Instagram</li>
+          <h3 className="footer-containerDiv-sectionTitle">Social Media</h3>
+          <ul className="footer-containerDiv-sectionList" id="social">
+            {/* <li className="footer-containerDiv-sectionListItem">Facebook</li>
+            <li className="footer-containerDiv-sectionListItem">Twitter</li>
+            <li className="footer-containerDiv-sectionListItem">Instagram</li> */}
+            <li
+              className="footer-containerDiv-sectionListItem"
+              id="social-list"
+            >
+              <a href="https://www.facebook.com">
+                <img src={Fb} alt="Facebook" />
+              </a>
+            </li>
+            <li
+              className="footer-containerDiv-sectionListItem"
+              id="social-list"
+            >
+              <a href="https://www.twitter.com">
+                <img src={Twitter} alt="Twitter" />
+              </a>
+            </li>
+            <li
+              className="footer-containerDiv-sectionListItem"
+              id="social-list"
+            >
+              <a href="https://www.instagram.com">
+                <img src={Insta} alt="Instagram" />
+              </a>
+            </li>
           </ul>
         </div>
       </div>
